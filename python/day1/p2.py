b136c01c44b4c6f40e6c2f1986faf6bc37f59e0f
David Anderson <danderson@beta.team>
2023-12-01 21:31:06 -0500

    descriptive names

--- python/day1/p2.py
+++ python/day1/p2.py
@@ -34,19 +34,19 @@ def string_ints_to_ints(my_str: LiteralString) -> LiteralString:
         my_str (LiteralString): A string containing integers both in digit and
         written form (i.e. '7' and 'seven')
     """
-    first_char = 0
-    second_char = 1
+    first_char_idx = 0
+    end_char_idx = 1
     to_return: tuple[LiteralString, int] = []
-    while (first_char + second_char) <= len(my_str):
-        while (first_char + second_char) <= len(my_str):
-            substring = my_str[first_char : (first_char + second_char)]
+    while (first_char_idx + end_char_idx) <= len(my_str):
+        while (first_char_idx + end_char_idx) <= len(my_str):
+            substring = my_str[first_char_idx : (first_char_idx + end_char_idx)]
             if substring in DAY_1_P2_STRING_INTS:
-                to_return.append((DAY_1_P2_STRING_INTS[substring], first_char))
+                to_return.append((DAY_1_P2_STRING_INTS[substring], first_char_idx))
             elif substring.isnumeric():
-                to_return.append((substring, first_char))
-            first_char += 1
-        first_char = 0
-        second_char += 1
+                to_return.append((substring, first_char_idx))
+            first_char_idx += 1
+        first_char_idx = 0
+        end_char_idx += 1
 
     to_return = sorted(to_return, key=itemgetter(1))
     return "".join([item[0] for item in to_return])
